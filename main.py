369dc41dd1aa674af48bf349b019895469a82831
Vredon6er <101344191+Vredon6er@users.noreply.github.com>
2022-04-14 15:18:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-dataframe = pd.read_csv('Capstone Data.csv', sep=';', header=0) # hier het pad naar je file zetten
+dataframe = pd.read_csv('/Users/joren/Downloads/run_7_data.csv', sep=';', header=0,decimal=',') # hier het pad naar je file zetten
 print(dataframe)
 
 ############### meerdere run's in een csv file: zet hier je run nr. anders is dit 1
-run = 1 # als je meerdere run's in een file hebt
-run_i = dataframe.iloc[:, run*3-3:run*3] # doe hier *2 en -2 als je alleen maar positie en intensiteit hebt en niet de tijd.
+
+run_i = dataframe.iloc[:, :]
 run_i = run_i.dropna()
 print(run_i)
 
 data = run_i.to_numpy(copy=True)
 data = np.transpose(data)
-array1 = data[0]  #deze waarde is 0,1 of 2 (afhankelijk van hoe de data file er uit ziet)
-array2 = data[1]  #deze waarde is 0,1 of 2 (afhankelijk van hoe de data file er uit ziet)
-plt.scatter(array1,array2,marker=".")
+array1 = data[0]
+array2 = data[1]
+
+fig, ax = plt.subplots()
+ax.scatter(array1, array2, marker=".")
+#ax.set( xticks=np.linspace(0, 350,10), yticks=np.linspace(0, 1.5,10))
 plt.show()
-
-
